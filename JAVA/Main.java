be6c7dec8b09eb7d6270782690c848ee7ef0fbe1
Vikash-Android7500 <Vikash75.jm@gmail.com>
2024-02-19 23:00:45 +0530

    first commet

---
+++ JAVA/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+     public static void main(String[] args) {
+          int Number = 10;
+          int Number2 = 20;
+
+          int sum = (Number + Number2);
+          System.out.println(" Your Sum: " + sum);
+     }
+}
