b1fcbc96181595c0dd96f986d1327273f4cafca5
Vikash-Android7500 <Vikash75.jm@gmail.com>
2024-02-21 23:38:30 +0530

    new Program

--- JAVA/Namaste.java
+++ JAVA/Namaste.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Namaste {
      public static void main(String[] args) {
           int Number = 10;
           int Number2 = 20;
---
+++ JAVA/Varible.java
@@ -0,0 +1,25 @@
+public class Varible {
+     public static void main(String[] args) {
+          byte item1 = 12;
+          short item2 = 13;
+          int item3 = 14;
+          float item4 = 15.0f;
+          long item5 = 16;
+          double item6 = 17.20;
+          char item7 = 'V';
+          String Name = "Vikash";
+          boolean item8 = true;
+
+          System.out.println(
+               "\n" + "itme1 "  + item1 +
+               "\n" + "itme2 "  + item2 +
+               "\n" + "itme3 "  + item3 +
+               "\n" + "itme4 "  + item4 +
+               "\n" + "itme5 "  + item5 +
+               "\n" + "itme6 "  + item6 +
+               "\n" + "itme7 "  + item7 +
+               "\n" + "Name: "  + Name +
+               "\n" + "itme8 "  + item8
+               );
+     }
+}
